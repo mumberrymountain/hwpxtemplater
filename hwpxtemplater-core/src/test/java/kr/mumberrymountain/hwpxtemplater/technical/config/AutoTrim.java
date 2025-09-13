b6c38531c111a9f3c9ab0804fa4a76789f021a3c
Jeon <mumberrymountain@gmail.com>
2025-09-14 00:36:16 +0900

    feat: config 옵션 ConfigOption Enum 클래스로도 설정 가능하도록 개선 (#21)

--- hwpxtemplater-core/src/test/java/kr/mumberrymountain/hwpxtemplater/technical/config/AutoTrim.java
+++ hwpxtemplater-core/src/test/java/kr/mumberrymountain/hwpxtemplater/technical/config/AutoTrim.java
@@ -3,6 +3,7 @@ package kr.mumberrymountain.hwpxtemplater.technical.config;
 import kr.dogfoot.hwpxlib.object.content.section_xml.paragraph.T;
 import kr.dogfoot.hwpxlib.tool.finder.ObjectFinder;
 import kr.mumberrymountain.hwpxtemplater.Config;
+import kr.mumberrymountain.hwpxtemplater.ConfigOption;
 import kr.mumberrymountain.hwpxtemplater.HWPXTemplater;
 import kr.mumberrymountain.hwpxtemplater.model.Text;
 import kr.mumberrymountain.hwpxtemplater.model.table.Col;
@@ -21,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AutoTrim {
     @Test
-    @DisplayName("1. autoTrim Configuration 옵션 적용")
+    @DisplayName("1. autoTrim Configuration 옵션 적용 - config 문자열로 설정")
     public void configAutoTrimTest() throws Exception {
 
         ArrayList<HashMap<String, Object>> trimLoops = new ArrayList<>();
@@ -53,6 +54,62 @@ public class AutoTrim {
 
         HWPXTemplater hwpxTemplater = HWPXTemplater.builder()
                 .config("autoTrim", true)
+                .parse(TestUtil.getFilePath(this.getClass(), "hwpx/config/Config_AutoTrim.hwpx"))
+                .render(new HashMap<String, Object>() {{
+                    put("AutoTrim1", "    공백 제거1     ");
+                    put("AutoTrim2", new Text("     공백 제거2     "));
+                    put("AutoTrimLoop", trimLoops);
+                    put("AutoTrimTable", tableParam);
+                }});
+
+        ObjectFinder.Result[] results = ObjectFinder.find(hwpxTemplater.getFile(), new ParaTFilter(), false);
+        ArrayList<String> texts = new ArrayList<>();
+        List<String> expectedResults = Arrays.asList(
+                "공백 제거1", "공백 제거2", "반복문 공백1", "반복문 공백2",
+                "반복문 공백3", "테이블 공백 제거1", "테이블 공백 제거 2"
+        );
+
+        for (int i = 0; i < results.length; i++) {
+            T text = (T) results[i].thisObject();
+            texts.add(text.onlyText());
+        }
+
+        assertEquals(texts, expectedResults);
+    }
+
+    @Test
+    @DisplayName("2. autoTrim Configuration 옵션 적용 - config ConfigOption Enum으로 설정")
+    public void configAutoTrimTestConfigOptEnum() throws Exception {
+
+        ArrayList<HashMap<String, Object>> trimLoops = new ArrayList<>();
+
+        HashMap<String, Object> trimLoop1 = new HashMap<>();
+        trimLoop1.put("TrimLoop", "   반복문 공백1     ");
+        HashMap<String, Object> trimLoop2 = new HashMap<>();
+        trimLoop2.put("TrimLoop", "   반복문 공백2     ");
+        HashMap<String, Object> trimLoop3 = new HashMap<>();
+        trimLoop3.put("TrimLoop", "   반복문 공백3     ");
+
+        trimLoops.add(trimLoop1);
+        trimLoops.add(trimLoop2);
+        trimLoops.add(trimLoop3);
+
+        Table tableParam = Table.builder()
+                .cols(
+                        Arrays.asList(
+                                new Col("공백제거").width(150)
+                        )
+                )
+                .row(new HashMap<String, Object>() {{
+                    put("공백제거", "   테이블 공백 제거1    ");
+                }})
+                .row(new HashMap<String, Object>() {{
+                    put("공백제거", "   테이블 공백 제거 2  ");
+                }})
+                .create();
+
+        HWPXTemplater hwpxTemplater = HWPXTemplater.builder()
+                .config(ConfigOption.AUTO_TRIM, true)
                 .parse(TestUtil.getFilePath(this.getClass(), "hwpx/config/Config_AutoTrim.hwpx"))
                 .render(new HashMap<String, Object>() {{
                     put("AutoTrim1", "    공백 제거1     ");
--- hwpxtemplater-core/src/test/java/kr/mumberrymountain/hwpxtemplater/technical/config/DelimPara.java
+++ hwpxtemplater-core/src/test/java/kr/mumberrymountain/hwpxtemplater/technical/config/DelimPara.java
@@ -3,6 +3,7 @@ package kr.mumberrymountain.hwpxtemplater.technical.config;
 import kr.dogfoot.hwpxlib.object.content.section_xml.paragraph.T;
 import kr.dogfoot.hwpxlib.tool.finder.ObjectFinder;
 import kr.mumberrymountain.hwpxtemplater.Config;
+import kr.mumberrymountain.hwpxtemplater.ConfigOption;
 import kr.mumberrymountain.hwpxtemplater.HWPXTemplater;
 import kr.mumberrymountain.hwpxtemplater.util.ParaTFilter;
 import kr.mumberrymountain.hwpxtemplater.util.TestUtil;
@@ -18,12 +19,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DelimPara {
     @Test
-    @DisplayName("1. delimPrefix, delimSuffix Configuration 옵션 적용")
+    @DisplayName("1. delimPrefix, delimSuffix Configuration 옵션 적용 - config 문자열로 설정")
     public void configDelimOneCharTest() throws Exception {
 
         HWPXTemplater hwpxTemplater = HWPXTemplater.builder()
                 .config("delimPrefix", "{")
                 .config("delimSuffix", "}")
+                .parse(TestUtil.getFilePath(this.getClass(), "hwpx/config/Config_Delim_OneChar.hwpx"))
+                .render(new HashMap<String, String>() {{
+                    put("Test1", "테스트1");
+                    put("Test2", "테스트2");
+                    put("Test3", "테스트3");
+                    put("Test4", "테스트4");
+                    put("Test5", "테스트5");
+                    put("Test6", "테스트6");
+                    put("Test7", "테스트7");
+                    put("Test8", "테스트8");
+                    put("Test9", "테스트9");
+                }});
+
+        ObjectFinder.Result[] results = ObjectFinder.find(hwpxTemplater.getFile(), new ParaTFilter(), false);
+        ArrayList<String> texts = new ArrayList<>();
+        List<String> expectedResults = Arrays.asList(
+                "테스트1", "테스트_", "테스트2", "_테스트",
+                "테스트3", "테스트4", "테스트5", "테스트6",
+                "테스트7", "테스트8", "테스트9"
+        );
+
+        for (int i = 0; i < results.length; i++) {
+            T text = (T) results[i].thisObject();
+            texts.add(text.onlyText());
+        }
+
+        assertEquals(texts, expectedResults);
+    }
+
+    @Test
+    @DisplayName("1. delimPrefix, delimSuffix Configuration 옵션 적용 - config ConfigOption Enum으로 설정")
+    public void configDelimOneCharTestConfigOptionEnum() throws Exception {
+
+        HWPXTemplater hwpxTemplater = HWPXTemplater.builder()
+                .config(ConfigOption.DELIM_PREFIX, "{")
+                .config(ConfigOption.DELIM_SUFFIX, "}")
                 .parse(TestUtil.getFilePath(this.getClass(), "hwpx/config/Config_Delim_OneChar.hwpx"))
                 .render(new HashMap<String, String>() {{
                     put("Test1", "테스트1");
